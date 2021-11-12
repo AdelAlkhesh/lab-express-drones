231f427d9007202c022460ac3a996804c63e2e15
AdelAlkhesh <splinteredsanity@outlook.com>
2021-11-12 17:12:08 +0100

    edited message

--- routes/drones.js
+++ routes/drones.js
@@ -4,6 +4,9 @@ const router = express.Router();
 // require the Drone model here
 const DroneModel = require('../models/Drone.model')
 
+//NOTE TO WHOEVER LOOKS AT THIS!!!! 
+//Sorry this website looks like crap, but I'm not a designer and probably never will be. I'm hoping that in the real world, the company I work at will just have a designer who hands me ready to make templates that I can just clone in terms of design... 
+
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   DroneModel.find()
